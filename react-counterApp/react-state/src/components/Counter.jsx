07171811255bc1860df8189e518dd45e93256f93
Almamy_IT_Training <Almamy_IT_Training@Almamys-MBP.home>
2024-05-14 09:47:15 -0400

    counter factor

--- react-counterApp/react-state/src/components/Counter.jsx
+++ react-counterApp/react-state/src/components/Counter.jsx
@@ -7,15 +7,27 @@ export default Counter;
 function Counter () {
 
     const [counter, setCounter] = useState(0);
+    const [incrementBy, setIncrementBy] = useState(1);
 
     const increment = function increment () {
 
-        return setCounter(counter + 1);
+        return setCounter(counter + incrementBy);
     }
 
     const decrement = function decrement () {
 
-        return setCounter(counter - 1);
+        return setCounter(counter - incrementBy);
+    }
+
+    const handleInput = function userInput (event) {
+
+        setIncrementBy(Number(event.target.value));
+        event.preventDefault();
+    }
+
+    const handleIncrementFactor = function factor () {
+
+        return setIncrementBy(incrementBy);
     }
 
     const clear = function clear () {
@@ -31,6 +43,11 @@ function Counter () {
             <button className="increment" onClick={increment}>Increment</button>
             <button className="clear" onClick={clear}>Clear</button>
             <button className="decrement" onClick={decrement}>Decrement</button>
+        </div>
+        <div className="operations">
+            <p>Increment/Decrement factor:</p>
+            <input type="number" value={incrementBy} onChange={handleInput} />
+            <button className="factor" type="submit" onClick={handleIncrementFactor}>Submit</button>
         </div>
         
     </div>
